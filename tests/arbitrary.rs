bdb6c4f37a309060f155cf3a8ee214e20e057d6f
Graydon Hoare <graydon@pobox.com>
2025-03-26 16:07:48 -0700

    Add test that serial order matches structured order

--- tests/arbitrary.rs
+++ tests/arbitrary.rs
@@ -1,7 +1,8 @@
 #![cfg(all(feature = "next", feature = "arbitrary"))]
 
 use arbitrary::{Arbitrary, Unstructured};
-use stellar_xdr::next::ScMap;
+use rand::RngCore;
+use stellar_xdr::next::{ScMap,ScVal,Limits,WriteXdr};
 
 #[test]
 fn arb() {
@@ -10,5 +11,28 @@ fn arb() {
     for _ in 1..10 {
         let x: ScMap = ScMap::arbitrary(&mut unstructured).unwrap();
         eprintln!("{x:?}");
+    }
+}
+
+#[test]
+fn serial_order_is_structured_order() {
+    let mut rng = rand::thread_rng();
+    let mut bytes: Vec<u8> = vec![0; 10000];
+    for _ in 1..10000 {
+        rng.fill_bytes(&mut bytes);
+        let mut unstructured = Unstructured::new(&bytes);
+        let x: ScVal = ScVal::arbitrary(&mut unstructured).unwrap();
+        let y: ScVal = ScVal::arbitrary(&mut unstructured).unwrap();
+        let xdr_x = x.to_xdr(Limits::none()).unwrap();
+        let xdr_y = y.to_xdr(Limits::none()).unwrap();
+        let x_lt_y = x < y;
+        let xdrx_lt_xdry = xdr_x < xdr_y;
+        if x_lt_y != xdrx_lt_xdry {
+            dbg!(x);
+            dbg!(y);
+            dbg!(xdr_x);
+            dbg!(xdr_y);
+            panic!("x < y ({x_lt_y}) != xdr_x < xdr_y ({xdrx_lt_xdry})");
+        }
     }
 }
